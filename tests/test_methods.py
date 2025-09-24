bb5f3a3ee239e4d6700fb0dd6f18f0c4c16087ab
Kat <cpnguyen0000@gmail.com>
2025-09-24 13:31:31 -0600

    Attempting to fix it without having to introduce an src file structure

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,7 +1,7 @@
-from water_column_resampling import water_column_resample
+import water_column_resampling
 
 def test_open():
-    x = water_column_resample("noaa-wcsd-zarr-pds/level_2/Henry_B._Bigelow/HB1906/EK60/HB1906.zarr/")
+    x = water_column_resampling.water_column_resample("noaa-wcsd-zarr-pds/level_2/Henry_B._Bigelow/HB1906/EK60/HB1906.zarr/")
     x.open_store()
     assert x.return_attributes() is not None
     assert x.return_shape() is not None
