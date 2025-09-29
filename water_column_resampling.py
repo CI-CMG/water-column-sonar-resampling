c71583ca03d22e57ea668130da4a6da6f830667d
Kat <cpnguyen0000@gmail.com>
2025-09-29 12:52:09 -0600

    Pushing to continue testing on lab computer-- added new dataarray initilizing

--- water_column_resampling.py
+++ water_column_resampling.py
@@ -1,7 +1,9 @@
 import xarray as xr
 import s3fs
 import json
-import numcodecs
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 # Can change method name later on
 class water_column_resample:
@@ -12,7 +14,7 @@ class water_column_resample:
         self.data_set = None
         self.attributes = None
 
-    # Actually opens the zarr store based in the link given
+    # Actually opens the zarr store based on the link given
     def open_store(self):
         self.store = s3fs.S3Map(root=self.store_link, s3=self.file_system)
         self.data_set = xr.open_zarr(store=self.store, consolidated=True)
@@ -39,8 +41,8 @@ class water_column_resample:
 
         else: # Returns default dimensions of the dataset
             return json.dumps(dict(self.data_set.sizes), indent=2) # Prints the shape of the data
-
-    # Creates a copy of the sv data from the store opened    
+        
+    # Creates a local copy of the sv data (complete sv, depth, time and frequency)
     def copy_sv_data(self):
         if self.data_set is None:
             self.open_store() # Opens the store if it hasn't been opened yet
@@ -55,10 +57,43 @@ class water_column_resample:
 
         # Pulling the sv data from the cloud store
         sv_data = cloud_store[['Sv']]
-        sv_data = sv_data.chunk({'depth': 512, 'time': 512, 'frequency': 1}) # Re-chunking to optimize performance
-        
+        sv_data = sv_data.chunk({'depth': 512, 'time': 512, 'frequency': 1})
+
         # Writing the sv data to the local store (copies the following data arrays: Sv, frequency, time, depth)
         local_store = sv_data.to_zarr('local_sv_data.zarr', mode='w', compute=True, zarr_format=2)
+
+    # Creates a new dataarray with just depth and time-- copies is locally   
+    def new_dataarray(self):
+        if self.data_set is None:
+            self.open_store() # Opens the store if it hasn't been opened yet
+        
+        # This opens the store from the cloud servers
+        cloud_store = self.data_set
+        cloud_store = cloud_store.chunk({'depth': 512, 'time': 512})
+
+        # Pulling specific data from the cloud store
+        depth = cloud_store['depth'].values
+        time = cloud_store['time'].values
+
+        # This opens a local zarr store to write to
+        local_store = xr.Dataset(
+            {
+                "Sv": xr.DataArray(
+                    data=np.empty((len(depth), len(time)), dtype='int16'),
+                    dims=("depth", "time"),
+                    coords={"depth": depth, "time": time}
+                )
+            }
+        )
+
+        local_store = local_store.chunk({'depth': 512, 'time': 512}) # Re-chunking to optimize performance
+
+        local_store['Sv'].plot() # Quick plot to see if it works-- can be removed later
+
+        plt.show()
+        
+        # Writing the sv data to the local store (copies the following data arrays: Sv, frequency, time, depth)
+        # local_store = local_store.to_zarr('local_dataarray.zarr', mode='w', compute=True, zarr_format=2)
 
     # TODO: Make it all close cleanly-- later goal
     def close(self):
@@ -67,4 +102,4 @@ class water_column_resample:
 # A test to see if it works-- use as needed
 if __name__ == "__main__":
     x = water_column_resample("noaa-wcsd-zarr-pds/level_2/Henry_B._Bigelow/HB0707/EK60/HB0707.zarr/")
-    x.copy_sv_data()
+    x.new_dataarray()
