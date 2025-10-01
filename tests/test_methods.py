edf48d35a9676911cbbe96fc0c386cdc9c68fc4c
Caitlyn Nguyen <cpgnuyen0000@gmail.com>
2025-10-01 13:51:58 -0600

    Adjusted the file structure so non-dummy tests work! We can change "wrc_workspace" later-- just needed a name for it

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,8 +1,7 @@
-# import water_column_resampling
+import wrc_workspace.water_column_resampling as wcr
 
 def test_open():
-    pass
-    """x = water_column_resampling.water_column_resample("noaa-wcsd-zarr-pds/level_2/Henry_B._Bigelow/HB1906/EK60/HB1906.zarr/")
+    x = wcr.water_column_resample("noaa-wcsd-zarr-pds/level_2/Henry_B._Bigelow/HB1906/EK60/HB1906.zarr/")
     x.open_store()
     assert x.return_attributes() is not None
-    assert x.return_shape() is not None"""
+    assert x.return_shape() is not None
