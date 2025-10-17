c48a4be6e30a0d5e3777c0535bd8729612a73890
Kat <cpnguyen0000@gmail.com>
2025-10-17 12:57:57 -0600

    Making an empty data tree based on the # of levels works! Just had to add it into the main package

--- wrc_workspace/dev.py
+++ wrc_workspace/dev.py
@@ -4,7 +4,7 @@
 # TODO:
 # REMINDER: THIS METHOD IS MEANT TO 1) DETERMINE # OF ZOOM LEVELS AND 2) MAKE A DATA TREE BASED ON THAT
 # [X] Actually pull out the time dimension from the zarr store
-# [] Determine number of zoom levels based on the time dimension (work in powers of 2)
+# [X] Determine number of zoom levels based on the time dimension (work in powers of 2)
 #      - Use the time dimension as the base dimension
 #      - Get the lowest level as close to 4096 as possible (define some kind of acceptable range)
 #      - Each zoom level would be half the previous level (use a for or until loop to do this and count the number of levels)
@@ -19,25 +19,8 @@ import numpy as np
 import tqdm
 import zarr
 
-# Write a new method to get dimensions!!!
-def get_dimension(self, variable=None):
-    # It's similar to return_shape but only for time dimension/coord
-    if self.data_set is None:
-        self.open_store() # Opens the store if it hasn't been opened yet
-    
-    if variable: # Processes a specific variable if one is given
-        if variable in self.data_set.coords:
-                var_dims = dict(zip(self.data_set[variable].dims, self.data_set[variable].shape))
-                return json.dumps({f"{variable}_dimensions": var_dims}, indent=2)
-        else:
-                return json.dumps({"error": f"Variable '{variable}' not found in dataset"}, indent=2)
-
-    else: # Returns default dimensions of the dataset
-            return json.dumps(dict(self.data_set.sizes), indent=2) # Prints the shape of the data
-    
-    # The actually working method has been added to water_column_resampling.py
-    pass
-
+# Might want to define a range above and below 4096 since it just has to be close, not exact
+# Alter as needed when placed inside the class
 def determine_zoom_levels(time_dim, base_size=4096):
     zoom_levels = 0
     current_size = time_dim
@@ -47,3 +30,24 @@ def determine_zoom_levels(time_dim, base_size=4096):
         zoom_levels += 1
 
     return zoom_levels, current_size
+        # zoom levels = # of branches/nodes in the tree
+
+def make_tree(levels):
+    tree = xr.DataTree(name="level_0")
+    current_node = tree
+
+    # For loop to continuously add levels
+    for level in range(1, levels + 1):
+        name = f"level_{level}"
+        current_node[name] = xr.DataTree(name=name)
+        current_node = current_node[name]
+        
+    for node in tree.subtree:
+        print(f"  {node.path}")
+    
+    return tree
+
+tree = make_tree(4)
+
+# make_tree(4)
+# print(xr.__version__)
